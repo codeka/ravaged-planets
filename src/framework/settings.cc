b355cf56f1b95c80b5ed4739c49179b5a3a7dd30
Dean Harding <dean@codeka.com.au>
2015-08-23 22:19:20 +1000

    Added a setting for dbghelp-path, which is used on Windows.

--- src/framework/settings.cc
+++ src/framework/settings.cc
@@ -76,6 +76,7 @@ void settings::initialize(po::options_description const &additional_options, int
       ("debug-logfile", po::value<std::string>()->default_value(""), "Name of the file to do debug logging to. If not specified, does not log.")
       ("debug-console", po::value<bool>()->default_value(true), "If set, we'll log to the console as well as the log file.")
       ("debug-libcurl", po::value<bool>()->default_value(false), "If true, debug HTTP requests and responses.")
+      ("dbghelp-path", po::value<std::string>()->default_value(""), "Windows-only, path to dbghelp.dll file.")
     ;
 
   po::options_description other_options("Other options");
