5af838da9f51e3ea4ded2eb8b77ec88a790b5f4c
Dean Harding <dean@codeka.com.au>
2015-09-06 08:23:52 +1000

    Added some basic error checking to the audio manager.

--- include/framework/audio.h
+++ include/framework/audio.h
@@ -1,14 +1,23 @@
 #pragma once
 
+#include <framework/exception.h>
+
 namespace fw {
+
+// this is the type of error info we include with exception's when an error is detected by SDL_mixer
+typedef boost::error_info<struct tag_audioerr, std::string> audio_error_info;
+
+// converts an audio_error_info into a string
+std::string to_string(audio_error_info const &err_info);
 
 class audio_manager {
 private:
 public:
-
   void initialize();
   void destroy();
   void update();
+
+  static void check_error(int error_code, char const *fn_name);
 };
 
 }
--- src/framework/audio.cc
+++ src/framework/audio.cc
@@ -7,13 +7,21 @@
 
 namespace fw {
 
+#if defined(_DEBUG)
+#define CHECK_ERROR(fn) \
+  audio_manager::check_error(fn, #fn)
+#else
+#define CHECK_ERROR(fn) \
+  fn
+#endif
+
 void audio_manager::initialize() {
   if (Mix_Init(MIX_INIT_OGG) != MIX_INIT_OGG) {
     BOOST_THROW_EXCEPTION(fw::exception() << fw::message_error_info("Error initializing SDL_mixer"));
   }
 
   // TODO: these are all just guesses (paraticularly frequency and chunksize)
-  Mix_OpenAudio(22050 /* frequency */, AUDIO_S16SYS /* format */, 2 /* channels */, 1024 /* chunksize */);
+  CHECK_ERROR(Mix_OpenAudio(22050 /* frequency */, AUDIO_S16SYS /* format */, 2 /* channels */, 1024 /* chunksize */));
 
   SDL_version const *link_version = Mix_Linked_Version();
   fw::debug << "SDL_mixer ("
@@ -28,7 +36,19 @@ void audio_manager::destroy() {
 }
 
 void audio_manager::update() {
+}
 
+void audio_manager::check_error(int error_code, char const *fn_name) {
+  if (error_code != 0) {
+    char *error_msg = Mix_GetError();
+    BOOST_THROW_EXCEPTION(fw::exception() << fw::message_error_info(fn_name) << fw::audio_error_info(error_msg));
+  }
+}
+
+//-----------------------------------------------------------------------------
+std::string to_string(audio_error_info const &err_info) {
+  std::string msg = err_info.value();
+  return msg;
 }
 
 }
