975789600a9a33d3c884c30f663de4034a0d075a
Dean Harding <dean@codeka.com.au>
2015-09-06 08:27:59 +1000

    Print out the actual values we initialized with.

--- src/framework/audio.cc
+++ src/framework/audio.cc
@@ -23,11 +23,28 @@ void audio_manager::initialize() {
   // TODO: these are all just guesses (paraticularly frequency and chunksize)
   CHECK_ERROR(Mix_OpenAudio(22050 /* frequency */, AUDIO_S16SYS /* format */, 2 /* channels */, 1024 /* chunksize */));
 
+  int frequency;
+  uint16_t format;
+  int channels;
+  CHECK_ERROR(Mix_QuerySpec(&frequency, &format, &channels));
+
+  std::string format_name;
+  switch(format) {
+    case AUDIO_U8: format_name = "U8"; break;
+    case AUDIO_S8: format_name = "S8"; break;
+    case AUDIO_U16LSB: format_name = "U16LSB"; break;
+    case AUDIO_S16LSB: format_name = "S16LSB"; break;
+    case AUDIO_U16MSB: format_name = "U16MSB"; break;
+    case AUDIO_S16MSB: format_name = "S16MSB"; break;
+    default: format_name = "<unknown>"; break;
+  }
+
   SDL_version const *link_version = Mix_Linked_Version();
   fw::debug << "SDL_mixer ("
       << static_cast<int>(link_version->major) << "."
       << static_cast<int>(link_version->minor) << "."
-      << static_cast<int>(link_version->patch) << ") initialized" << std::endl;
+      << static_cast<int>(link_version->patch) << ") initialized "
+      << "[" << frequency << "Hz, " << format_name << " " << channels << " channels]" << std::endl;
 }
 
 void audio_manager::destroy() {
