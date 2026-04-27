c058a23c9760ca11e4567960cd89dd72dfdce9b0
Dean Harding <dean@codeka.com>
2026-04-26 22:23:14 -0700

    Converted texture tool to the new system

--- src/game/editor/tools/texture_tool.cc
+++ src/game/editor/tools/texture_tool.cc
@@ -48,39 +48,68 @@ public:
   void hide();
 };
 
-TextureToolWindow::TextureToolWindow(ed::TextureTool &tool) : tool_(tool) {/*
-  wnd_ = Builder<Window>(px(10), px(30), px(100), px(262)) << Widget::background("frame")
-      << (Builder<Label>(px(4), px(4), sum(pct(100), px(-8)), px(18)) << Label::text("Size:"))
-      << (Builder<Slider>(px(4), px(26), sum(pct(100), px(-8)), px(18))
-          << Slider::limits(10, 100)
-          << Slider::on_update(std::bind(&TextureToolWindow::on_radius_updated, this, _1))
-          << Slider::value(40))
-      << (Builder<Listbox>(px(4), px(48), sum(pct(100), px(-8)), px(80))
-          << Widget::id(TEXTURES_ID)
-          << Listbox::item_selected(std::bind(&TextureToolWindow::on_texture_selected, this, _1)))
-      << (Builder<Label>(px(4), px(132), sum(pct(100), px(-8)), px(92))
-          << Widget::id(TEXTURE_PREVIEW_ID))
-      << (Builder<Button>(px(4), px(228), sum(pct(100), px(-8)), px(30))
-          << Button::text("Change"));
-  fw::Get<Gui>().attach_widget(wnd_);*/
+TextureToolWindow::TextureToolWindow(ed::TextureTool &tool) : tool_(tool) {
+  wnd_ = Builder<Window>()
+      << Widget::width(Widget::Fixed(100.f))
+      << Widget::height(Widget::WrapContent())
+      << Window::initial_position(WindowInitialPosition::Absolute(20.f, 40.f))
+      << Widget::background("frame")
+      << (Builder<LinearLayout>()
+          << Widget::width(Widget::MatchParent())
+          << Widget::height(Widget::WrapContent())
+          << LinearLayout::orientation(LinearLayout::Orientation::kVertical)
+          << (Builder<Label>()
+              << Widget::width(Widget::MatchParent())
+              << Widget::height(Widget::WrapContent())
+              << Widget::margin(10.f, 10.f, 5.0f, 10.0f)
+              << Label::text("Size:"))
+          << (Builder<Slider>()
+              << Widget::width(Widget::MatchParent())
+              << Widget::height(Widget::WrapContent())
+              << Widget::margin(5.f, 10.f, 5.0f, 10.0f)
+              << Slider::limits(10, 100)
+              << Slider::on_update(std::bind(&TextureToolWindow::on_radius_updated, this, _1))
+              << Slider::value(40))
+          << (Builder<Listbox>()
+              << Widget::width(Widget::MatchParent())
+              << Widget::height(Widget::Fixed(100.f))
+              << Widget::margin(5.f, 10.f, 5.0f, 10.0f)
+              << Widget::id(TEXTURES_ID)
+              << Listbox::item_selected(std::bind(&TextureToolWindow::on_texture_selected, this, _1)))
+          << (Builder<Label>()
+              << Widget::width(Widget::MatchParent())
+              << Widget::height(Widget::Fixed(80.f))
+              << Widget::margin(5.f, 10.f, 5.0f, 10.0f)
+              << Widget::id(TEXTURE_PREVIEW_ID))
+          << (Builder<Button>()
+              << Widget::width(Widget::MatchParent())
+              << Widget::height(Widget::WrapContent())
+              << Widget::margin(5.f, 10.f, 10.0f, 10.0f)
+              << Button::text("Change"))
+      );
+  fw::Get<Gui>().AttachWindow(wnd_);
 }
 
 TextureToolWindow::~TextureToolWindow() {
- // fw::Get<Gui>().detach_widget(wnd_);
+  fw::Get<Gui>().DetachWindow(wnd_);
 }
 
 void TextureToolWindow::show() {
   wnd_->set_visible(true);
 
-  auto lbx = wnd_->Find<Listbox>(TEXTURES_ID);
-  lbx->Clear();
-  for (int i = 0; i < tool_.get_terrain()->get_num_layers(); i++) {
-    fs::path filename = tool_.get_terrain()->get_layer(i).get_filename();/*
-    lbx->add_item(
-        Builder<Label>(px(0), px(0), pct(100), px(18))
-            << Label::text(filename.stem().string()));*/
-  }
-  lbx->SelectItem(0);
+  fw::Get<fw::Graphics>().run_on_render_thread([this]() {
+    auto lbx = wnd_->Find<Listbox>(TEXTURES_ID);
+    lbx->Clear();
+    for (int i = 0; i < tool_.get_terrain()->get_num_layers(); i++) {
+      fs::path filename = tool_.get_terrain()->get_layer(i).get_filename();
+      lbx->AddItem(
+        Builder<Label>()
+            << Widget::width(Widget::MatchParent())
+            << Widget::height(Widget::WrapContent())
+            << Label::text(filename.stem().string()));
+    }
+    lbx->SelectItem(0);
+  });
 }
 
 void TextureToolWindow::hide() {
